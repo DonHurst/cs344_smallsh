3b0c191e3681ccbfe5ed962b4c768fa40300229b
Don <hurstd@oregonstate.edu>
2022-02-01 14:08:57 -0800

    have the expand function working correctly

--- smallsh.c
+++ smallsh.c
@@ -38,6 +38,9 @@ struct command *getCommand() {
     char* args;
     args = (char *)malloc(MAX_LENGTH * sizeof(char));
 
+    // Set the background status to 0 by default
+    currCommand->backgroundStatus = 0;
+
     // Print the : and flush the buffer
     printf(": ");
     fflush(stdout);
@@ -80,11 +83,18 @@ struct command *getCommand() {
             strcpy(currCommand->inputFile, token);
         }
 
+
         // // If the token is >
         else if (strcmp(token, ">") == 0) {
             token = strtok(NULL, " ");
             currCommand->outputFile = calloc(strlen(token) + 1, sizeof(char));
             strcpy(currCommand->outputFile, token);
+        }
+
+        else if (strcmp(token, "&") == 0) {
+            token = strtok(NULL, " ");
+            currCommand->backgroundStatus = 1;
+            continue;
         }
         
         // // IF the token is &
@@ -97,6 +107,7 @@ struct command *getCommand() {
             printf("PRE - %s\n", token);
             // strcpy(currCommand->commandList, token);
             strcat(currCommand->commandList, token);
+            strcat(currCommand->commandList, " ");
             // token = strtok(NULL, " ");
             printf("POST - %s\n", token);
             // currCommand->outputFile = calloc(strlen(token) + 1, sizeof(char));
@@ -111,20 +122,100 @@ struct command *getCommand() {
         
     }
 
-    printf("inFile - %s\noutFile - %s\nCommands -  %s\n", currCommand->inputFile, currCommand->outputFile, currCommand->commandList);
+    // printf("inFile - %s\noutFile - %s\nCommands -  %s\nBackgroundStatus - %d", currCommand->inputFile, currCommand->outputFile, currCommand->commandList, currCommand->backgroundStatus);
 
     return currCommand;
 
 }
 
+void expand(struct command *currCommand, int pidnum) {
+
+    char temp;
+    char nextTemp;
+    char newString[MAX_ARGS];
+    char pidString[MAX_ARGS];
+    int signsFound;
+
+    sprintf(pidString, "%d", pidnum);
+
+    printf("The PID - %s\n", pidString);
+
+            // temp = token[i];
+            // nextTemp = token[i]+1;
+            
+
+            // printf("The Character - %c\n", temp);
+            // printf("The Next Character - %c\n\n", temp);
+            
+            // printf("Token - %c", token[i]);
+            // if ((strcmp(temp,"$") == 0) && strcmp(temp, "$") == 0) {
+            //     printf("In here!");
+            //     strcat(newString, pidString);
+            //     i += 1;
+            // }
+            // else {
+            //     strcat(newString, &temp[i]);
+            // }
+
+
+    // Tokenize commands string and iterate over it
+    char* token = strtok(currCommand->commandList, " ");
+    while (token) {
+        printf("The token! - %s\n", token);
+        // Loop over the string
+        for (int i = 0; i < strlen(token); i++) {
+            temp = token[i];
+            signsFound = 0;
+
+            // Nested loop over the string again
+            for(int j = 1; j < strlen(token); j++) {
+                nextTemp = token[j];
+
+                // If 
+                if ((temp == '$') && (nextTemp == '$') && (j-i == 1)) {
+                    signsFound = 1;
+                }
+
+                // printf("the temp - %c\n", temp);
+                // printf("the next temp - %c\n", nextTemp);
+                // If the current and next value are both $'s, replace with the pid
+                // if ((token[i] == "$") && (strcmp(token[j], "$")) && (j-i == 1)){
+                //     printf("They MAtch!");
+                // }
+            }
+            if (signsFound == 1) {
+                strcat(newString, pidString);
+                i += 1;
+            }
+            else {
+                strncat(newString, &token[i], 1);
+            }
+            
+            
+        }
+        // Advance to next command
+        token = strtok(NULL, " ");
+    }
+
+    printf("The New String - %s", newString);
+
+    // printf("\n The Temp - %s", temp);
+
+}
+
 int main() {
 
-    int i = 0;
-    do {
+    // int i = 0;
+
+    printf("%d", getpid());
+    // do {
 
     struct command *newCommand = getCommand();
-    printf("inFile - %s\noutFile - %s\nCommands -  %s\n", newCommand->inputFile, newCommand->outputFile, newCommand->commandList);
-    i++;
-    } while(i < 5);
+    // printf("inFile - %s\noutFile - %s\nCommands -  %s\nBackgroundStatus - %d", newCommand->inputFile, newCommand->outputFile, newCommand->commandList, newCommand->backgroundStatus);
+    // i++;
+    // } while(i < 5);
+    printf("\n The PID - %d", getpid());
+    expand(newCommand, getpid());
 }
+
 
