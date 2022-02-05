3f3f639141d5287816db750ae57935bcba91bbc7
Don <hurstd@oregonstate.edu>
2022-02-04 16:12:54 -0800

    needs work

--- smallsh.c
+++ smallsh.c
@@ -328,14 +328,14 @@ void createFork(struct command *currCommand) {
             }
             
         while((spawnpid = waitpid(-1, &childStatus, WNOHANG)) > 0) {
-            printf("Child %d termindated\n", spawnpid);
+            printf("Background pid %d is done: ", spawnpid);
             if (WIFEXITED(childStatus)) {
 		        // If exited by status
 		        printf("exit value %d\n", WEXITSTATUS(childStatus));
 	        } 
             else {
 		        // If terminated by signal
-		        printf("terminated by signal %d\n", WTERMSIG(childStatus));
+		        printf("exit value: %d\n", WTERMSIG(childStatus));
 	        }
         }
     }
